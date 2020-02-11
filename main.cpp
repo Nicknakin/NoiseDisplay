c210a6f3f8651ba1056e3c576fc65dad0ca7275c
Nick Kinney <kinneyanick@gmail.com>
2020-02-11 16:49:28 -0600

    Last commit before bed

--- main.cpp
+++ main.cpp
@@ -17,35 +17,35 @@ struct setting{
     const int speed;
 };
 
-
 int main(int argc, char** argv){
-    setting config = {(argc >= 2)? std::stoi(argv[1]): 1920/2, (argc >= 3)? std::stoi(argv[2]): 1080/2-1, (argc >= 4)? std::stoi(argv[3]): 10, (argc >= 5)? std::stoi(argv[4]): 60};
-  
+    setting config = {(argc >= 2)? std::stoi(argv[1]): 1920/2, (argc >= 3)? std::stoi(argv[2]): 1080/2, (argc >= 4)? std::stoi(argv[3]): 10, (argc >= 5)? std::stoi(argv[4]): 60};
+    
+    const int gridWidth = config.width/config.sideLength, gridHeight = config.height/config.sideLength;
+
     Perlin ng{std::vector<int>{255,255,255}};
 
-    std::vector<sf::RectangleShape> blocks{};
-    blocks.resize((config.width*config.height)/(config.sideLength*config.sideLength), sf::RectangleShape{sf::Vector2<float>{config.sideLength, config.sideLength}});
+    std::vector<sf::RectangleShape*> blocks{};
+    blocks.resize(gridWidth*gridHeight);
     
     //Create window with SFML and set framerate
     sf::RenderWindow window(sf::VideoMode(config.width,config.height), "SFML Project");
     window.setFramerateLimit(config.speed);
     for(int i = 0; i < blocks.size(); i++){
-        auto &block = blocks[i];
+        blocks[i] = new sf::RectangleShape{sf::Vector2<float>{(float) config.sideLength, (float) config.sideLength}};
+        auto &block = *blocks[i];
         int x = i%(config.width/config.sideLength)*config.sideLength, y = floor(i/(config.width/config.sideLength))*config.sideLength;
         block.setPosition(x, y);
         float xNoise = x/((float) config.width)*255, yNoise = y/((float) config.height)*255;
         float noise = ng(std::vector<float>{xNoise, yNoise, 0.5f});
-        int col = noise*255;
+        unsigned int col = noise*255;
         block.setFillColor(sf::Color{col});
     }
 
     while (window.isOpen()) {
-        
-        std::cout << ',';
+        window.clear();        
         for(auto &block : blocks){
-            window.draw(block);
+            window.draw(*block);
         }
-        std::cout << '.';
         window.display();
     }
     std::cout << std::endl;
