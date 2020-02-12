c93b7184010e1760ee8ac55b4edfde7e6ef5783a
Nick Kinney <kinneyanick@gmail.com>
2020-02-12 07:04:41 -0600

    Now displays noise properly, noise class also is fixed and pulled from source after bug fixes.

--- main.cpp
+++ main.cpp
@@ -32,18 +32,18 @@ int main(int argc, char** argv){
     window.setFramerateLimit(config.speed);
     for(int i = 0; i < blocks.size(); i++){
         blocks[i] = new sf::RectangleShape{sf::Vector2<float>{(float) config.sideLength, (float) config.sideLength}};
-        auto &block = *blocks[i];
-        int x = i%(config.width/config.sideLength)*config.sideLength, y = floor(i/(config.width/config.sideLength))*config.sideLength;
-        block.setPosition(x, y);
-        float xNoise = x/((float) config.width)*255, yNoise = y/((float) config.height)*255;
+        int x = i%(gridWidth)*config.sideLength, y = floor(i/gridWidth)*config.sideLength;
+        blocks[i]->setPosition(x, y);
+        blocks[i]->setSize(sf::Vector2f{(float) config.sideLength, (float) config.sideLength});
+        float xNoise = x/((float) config.width)*25, yNoise = y/((float) config.height)*25;
         float noise = ng(std::vector<float>{xNoise, yNoise, 0.5f});
         unsigned int col = noise*255;
-        block.setFillColor(sf::Color{col});
+        blocks[i]->setFillColor(sf::Color{col, col, col});
     }
 
     while (window.isOpen()) {
-        window.clear();        
-        for(auto &block : blocks){
+    window.clear();
+        for(auto block : blocks){
             window.draw(*block);
         }
         window.display();
