f6c4faf72dae42bfa3e90d6f6e79b9b611927660
Nicholas Kinney <KinneyANick@gmail.com>
2020-02-12 16:16:54 -0600

    Multi-threaded initialization of blocks

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <stdlib.h>
+#include <math.h>
+#include <algorithm>
 #include <iostream>
 #include <iomanip>
-#include <vector>
-#include <algorithm>
 #include <numeric>
-#include <math.h>
+#include <thread>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 
@@ -17,21 +18,10 @@ struct setting{
     const int speed;
 };
 
-int main(int argc, char** argv){
-    setting config = {(argc >= 2)? std::stoi(argv[1]): 1920/2, (argc >= 3)? std::stoi(argv[2]): 1080/2, (argc >= 4)? std::stoi(argv[3]): 10, (argc >= 5)? std::stoi(argv[4]): 60};
-    
+void setBlocks(std::vector<sf::RectangleShape*> blocks, int start, int end, struct setting config, Perlin* ngp){
+    Perlin& ng = *ngp;
     const int gridWidth = config.width/config.sideLength, gridHeight = config.height/config.sideLength;
-
-    Perlin ng{std::vector<int>{255,255,255}};
-
-    std::vector<sf::RectangleShape*> blocks{};
-    blocks.resize(gridWidth*gridHeight);
-    
-    //Create window with SFML and set framerate
-    sf::RenderWindow window(sf::VideoMode(config.width,config.height), "SFML Project");
-    window.setFramerateLimit(config.speed);
-    for(int i = 0; i < blocks.size(); i++){
-        blocks[i] = new sf::RectangleShape{sf::Vector2<float>{(float) config.sideLength, (float) config.sideLength}};
+    for(int i = start; i <end; i++){
         int x = i%(gridWidth)*config.sideLength, y = floor(i/gridWidth)*config.sideLength;
         blocks[i]->setPosition(x, y);
         blocks[i]->setSize(sf::Vector2f{(float) config.sideLength, (float) config.sideLength});
@@ -40,7 +30,40 @@ int main(int argc, char** argv){
         unsigned int col = noise*255;
         blocks[i]->setFillColor(sf::Color{col, col, col});
     }
+}
 
+int main(int argc, char** argv){
+    setting config = {(argc >= 2)? std::stoi(argv[1]): 1920/2, (argc >= 3)? std::stoi(argv[2]): 1080/2, (argc >= 4)? std::stoi(argv[3]): 10, (argc >= 5)? std::stoi(argv[4]): 60};
+    
+    const int gridWidth = config.width/config.sideLength, gridHeight = config.height/config.sideLength;
+    
+    Perlin ng{std::vector<int>{255,255,255}};
+
+    std::vector<sf::RectangleShape*> blocks{};
+    blocks.resize(gridWidth*gridHeight);
+    
+    //Create window with SFML and set framerate
+    sf::RenderWindow window(sf::VideoMode(config.width,config.height), "SFML Project");
+    window.setFramerateLimit(config.speed);
+   
+    for(int i = 0; i < blocks.size(); i++){
+        blocks[i] = new sf::RectangleShape{sf::Vector2<float>{(float) config.sideLength, (float) config.sideLength}};
+    }
+
+    std::vector<std::thread> activeThreads{};
+    unsigned threads = std::thread::hardware_concurrency();
+    if(threads == 0)
+        setBlocks(blocks, 0, blocks.size(), config, &ng);
+    else{
+        for(int i = 0; i < threads; i++){
+            activeThreads.push_back(std::thread(setBlocks, blocks, blocks.size()/threads*i, blocks.size()/threads*(i+1), config, &ng));
+        }
+    }   
+    for(int i = 0; i < activeThreads.size(); i++){
+        activeThreads[i].join();
+    }   
+    activeThreads.resize(0);
+   
     while (window.isOpen()) {
     window.clear();
         for(auto block : blocks){
